c621b62ff0e6b21c24ce18c0b8719139b8a5ad64
SaiPavan72 <p.saipavan143@gmail.com>
2021-05-15 19:13:14 +0530

    firstcommit

---
+++ team/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+class Team(models.Model):
+    team_name = models.CharField(max_length=100)
+    team_logo = models.ImageField(upload_to='media/')
+    club_state = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.team_name
+
+
+class Players(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    player_fname = models.CharField(max_length=100)
+    player_lname = models.CharField(max_length=100)
+    player_image = models.ImageField(upload_to='media1/')
+    jersey_number = models.IntegerField(null=False,blank=False)
+    player_country = models.CharField(max_length=100)
+    total_matches=models.IntegerField(default=0)
+    total_runs=models.IntegerField(default=0)
+    hundreds=models.IntegerField(default=0)
+    half_centuries=models.IntegerField(default=0)
+    highest_score=models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.player_fname
+
+class Matches_points(models.Model):
+    team_matches=models.Textfield(max_length=250)
+    points=models.IntegerField()
